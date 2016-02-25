a10d1b95f1ee55929cf76c433285230f49abde4e
Antun Novosel <Antun.Novosel@infobip.com>
2016-02-25 17:01:49 +0100

    adding support for update *.xliff with csv file

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ var parser = new xml2js.Parser();
 
 var arguments = process.argv;
 
+var lineReader = require('readline').createInterface({
+  input: fs.createReadStream('en.txt')
+});
+
+var lines = [];
+
+lineReader.on('line', function (line) {
+  lines.push(line);
+  console.log(lines.length);
+});
+
+
+lineReader.on('close', function() {
+  console.log('procitao sve!')
+})
+
+
+
+function jsToXml(jsObject) {
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(jsObject);
+  fs.writeFile(__dirname + '/en-updated.xliff', xml, function(err, result) {
+    if (err) {
+      console.log('nisi pazio :D');
+      console.log(err);
+    } else {
+      console.log('checkout your file');
+    }      
+  });
+}
+
 function processObject(object) {
   var files = object.xliff.file;
 
@@ -17,21 +48,26 @@ function processObject(object) {
             translationUnits.forEach(function(translationUnit){
               var keys = Object.keys(translationUnit);
               // console.log(translationUnit);
+              // make this more modular
               console.log(  translationUnit[keys[0]].id +
-                            ', ' +
+                            '| ' +
                             translationUnit[keys[1]] +
-                            ', ' +
+                            '| ' +
                             translationUnit[keys[2]]);
+              translationUnit[keys[1]] = "Prevod";
+              translationUnit[keys[2]] = "Description";
+
             });
           }
         }
       });
     }
-  });
+  })
+  console.log("____________====================____________");
+  jsToXml(object);
 }
-
-fs.readFile(__dirname + '/en.xliff', function(err, data) {
-    parser.parseString(data, function (err, result) {
-	processObject(result);
-    });
-});
+// fs.readFile(__dirname + '/en.xliff', function(err, data) {  
+//     parser.parseString(data, function (err, xmlObject) {
+//       processObject(xmlObject);
+//     });
+// });
